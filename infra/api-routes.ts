51877bccf903628547238395820ae95a965ef3ad
Owen Buckley <owenbuckley13@gmail.com>
2026-07-02 15:45:40 -0400

    feat: pull latest sst configurations

--- infra/api-routes.ts
+++ infra/api-routes.ts
@@ -2,21 +2,6 @@ export const api = new sst.aws.ApiGatewayV2("Api");
 
 const RUNTIME = "nodejs24.x";
 
-// TODO: get this from @greenwood/cli
-// function getDynamicPages(compilation) {
-//   const { config, graph } = compilation;
-
-//   return graph.filter((page) => {
-//     let isSsrRoute = page.isSSR && !page.staticPaths && page.prerender !== true;
-
-//     if (isSsrRoute && config.prerender && page.prerender !== false) {
-//       isSsrRoute = false;
-//     }
-
-//     return isSsrRoute;
-//   });
-// }
-
 // TODO need to handle basePath here?  (and / or all adapters?)
 // @ts-expect-error see https://github.com/microsoft/TypeScript/issues/42866
 const apiRoutes = (
@@ -25,14 +10,21 @@ const apiRoutes = (
 // @ts-expect-error see https://github.com/microsoft/TypeScript/issues/42866
 const ssrPages = (
   await import(new URL("../../public/graph.json", import.meta.url), { with: { type: "json" } })
-).default.filter((page) => page.isSSR && !page.staticPaths);
+).default.filter((page) => page.isSSR && !page.hasStaticParams);
 
 // https://sst.dev/docs/component/aws/apigatewayv2
 // https://sst.dev/docs/component/aws/function
+// NOTE: API Gateway routes can NOT end in a trailing /
 ssrPages.forEach((page) => {
-  const { id } = page;
+  const { id, segment, route } = page;
+  const routePattern = segment?.key
+    ? segment.pathname.replace(`:${segment.key}/`, "{proxy+}") // we use proxy+ to match everything, including trailing /
+    : `/${route
+        .split("/")
+        .filter((segment) => segment !== "")
+        .join("/")}`;
 
-  api.route(`GET /routes/${id}`, {
+  api.route(`GET /routes${routePattern}`, {
     bundle: `.aws-output/routes/${id}`,
     handler: "index.handler",
     runtime: RUNTIME,
@@ -42,7 +34,8 @@ ssrPages.forEach((page) => {
 apiRoutes.forEach((apiRoute) => {
   const [route, { id }] = apiRoute;
 
-  api.route(`ANY ${route}`, {
+  // swap out [] for {} in route for AWS API Gateway compatibility
+  api.route(`ANY ${route.replace("[", "{").replace("]", "}")}`, {
     bundle: `.aws-output/api/${id}`,
     handler: "index.handler",
     runtime: RUNTIME,
--- infra/router.ts
+++ infra/router.ts
@@ -1,23 +1,40 @@
-import { frontend } from "./static.ts";
-import { api } from "./api-routes.ts";
+import { frontend } from "./static";
+import { api } from "./api-routes";
 
 // @ts-expect-error see https://github.com/microsoft/TypeScript/issues/42866
 const ssrPages = (
   await import(new URL("../../public/graph.json", import.meta.url), { with: { type: "json" } })
-).default.filter((page) => page.isSSR && !page.staticPaths);
+).default.filter((page) => page.isSSR && !page.hasStaticParams);
 const ssrRoutes = {};
 
 // TODO handle base path
 ssrPages.forEach((page) => {
-  const { route, id } = page;
+  const { route, segment } = page;
 
-  ssrRoutes[page.route] = {
-    url: api.url,
-    rewrite: {
-      regex: `^${route}$`,
-      to: `/routes/${id}`,
-    },
-  };
+  if (segment?.key) {
+    const basePattern = segment.pathname.replace(`/:${segment.key}/`, "");
+
+    ssrRoutes[`${basePattern}/*`] = {
+      url: api.url,
+      rewrite: {
+        regex: `^${basePattern}/(.*)$`,
+        to: `/routes${basePattern}/$1`,
+      },
+    };
+  } else {
+    const routePattern = `/${route
+      .split("/")
+      .filter((segment) => segment !== "")
+      .join("/")}`;
+
+    ssrRoutes[route] = {
+      url: api.url,
+      rewrite: {
+        regex: `^${route}$`,
+        to: `/routes/${routePattern}`,
+      },
+    };
+  }
 });
 
 // https://sst.dev/docs/component/aws/router
