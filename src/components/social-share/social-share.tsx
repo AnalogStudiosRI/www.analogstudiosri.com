b95166f87a43b2f480814eaa86df92888748066c
Owen Buckley <owen@thegreenhouse.io>
2026-08-15 01:39:28 -0400

    enhancement: #21 get web social share types from package

--- src/components/social-share/social-share.tsx
+++ src/components/social-share/social-share.tsx
@@ -2,6 +2,10 @@ import socialShareSheet from "./social-share.css" with { type: "css" };
 import themeSheet from "../../styles/theme.css" with { type: "css" };
 import { defineCustomElement } from "web-social-share";
 
+// web social share's exports overrides its own types field
+// so have to craft it like this instead
+type ShareConfig = HTMLWebSocialShareElement["share"];
+
 declare global {
   namespace JSX {
     interface IntrinsicElements {
@@ -11,17 +15,6 @@ declare global {
       };
     }
   }
-}
-
-// TODO: get types from web-social-share package instead of defining our own here
-// https://github.com/AnalogStudiosRI/www.analogstudiosri.com/issues/21
-interface ShareConfig {
-  displayNames: boolean;
-  config: Array<{
-    [key: string]: {
-      socialShareUrl: string;
-    };
-  }>;
 }
 
 export default class SocialShare extends HTMLElement {
