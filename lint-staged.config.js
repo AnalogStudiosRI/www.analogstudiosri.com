bd595b599d92d2093f673abfd6e11b9b7096c840
Owen Buckley <owen@thegreenhouse.io>
2026-08-15 01:14:30 -0400

    chore: include ts files in lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 export default {
   "*.js": ["npm run lint:js --"],
+  "*.ts": ["npm run lint:ts --"],
   "*.css": ["npm run lint:css --"],
   // TODO: '*.ts': ['npm run check --'],
   // https://github.com/AnalogStudiosRI/www.analogstudiosri.com/issues/26
