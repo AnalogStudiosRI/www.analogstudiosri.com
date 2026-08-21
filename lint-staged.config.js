a2ea3e53a88a7b126c9c2bdbb1d763c57329ec1e
Owen Buckley <owenbuckley13@gmail.com>
2026-08-20 20:54:53 -0400

    chore: #26 restore type checking in lint staged (#34)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 export default {
   "*.js": ["npm run lint:js --"],
   "*.css": ["npm run lint:css --"],
-  // TODO: '*.ts': ['npm run check --'],
-  // https://github.com/AnalogStudiosRI/www.analogstudiosri.com/issues/26
+  // https://github.com/lint-staged/lint-staged#example-run-tsc-on-changes-to-typescript-files-but-do-not-pass-any-filename-arguments
+  "*.{ts,tsx}": [() => "npm run check"],
   "*.*": ["npm run lint:ls --", "npm run format --"],
 };
