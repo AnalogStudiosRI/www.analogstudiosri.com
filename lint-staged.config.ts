87c73b908671027c3ebde8f0301ee92522ec4c9f
Owen Buckley <owen@thegreenhouse.io>
2026-08-20 21:11:57 -0400

    chore: #7 convert tooling files to typescript

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,7 +1,9 @@
+import type { Configuration } from "lint-staged";
+
 export default {
   "*.js": ["npm run lint:js --"],
   "*.css": ["npm run lint:css --"],
   // https://github.com/lint-staged/lint-staged#example-run-tsc-on-changes-to-typescript-files-but-do-not-pass-any-filename-arguments
   "*.{ts,tsx}": [() => "npm run check"],
   "*.*": ["npm run lint:ls --", "npm run format --"],
-};
+} satisfies Configuration;
